b684d226e7f6082af4dee091447710068c3c543f
Joao Paulo <js0918990@gmail.com>
2025-11-19 23:21:02 -0300

    cor

--- src/Home.jsx
+++ src/Home.jsx
@@ -122,7 +122,7 @@ function Home() {
                     <div className="w-6"></div>
                 </nav>
 
-                <nav className="fixed top-0 w-full h-14 bg-orange-950 px-6 flex items-center justify-between z-[99999] shadow">
+                <nav className="fixed top-52 w-full h-14 bg-orange-500 px-6 flex items-center justify-between z-[99] shadow">
 
                     {/* Ícone Hamburguer */}
                     <div onClick={() => setOpen(true)} className="space-y-1.5 cursor-pointer ">
