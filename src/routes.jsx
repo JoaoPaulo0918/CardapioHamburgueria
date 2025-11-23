ada610ecc47ea1dc621f85264cfc8cb4aa7b9e31
Joao Paulo <js0918990@gmail.com>
2025-11-23 00:01:33 -0300

    codigo 1

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Cardapio from "./cardapio";
+import Cardapio from "./Cardapio";
 import Home from "./Home";
 import Pedido from "./Pedidos";
 import './index.css'
@@ -10,7 +10,7 @@ function RoutesApp() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cardapio" element={<Cardapio key={Date.now()} />} />
+      <Route path="/Cardapio" element={<Cardapio key={Date.now()} />} />
       <Route path="/Pedidos/:id" element={<Pedido />} />
     </Routes>
   );
